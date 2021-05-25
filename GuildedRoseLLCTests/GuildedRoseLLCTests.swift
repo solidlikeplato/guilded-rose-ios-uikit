8ce580a92b41bab651993072c9d571f8ea5f2da9
solidlikeplato <ethanwhaley@gmail.com>
2021-05-25 15:09:34 -0500

    failing test

--- GuildedRoseLLCTests/GuildedRoseLLCTests.swift
+++ GuildedRoseLLCTests/GuildedRoseLLCTests.swift
@@ -21,6 +21,7 @@ class GuildedRoseLLCTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(false)
     }
 
     func testPerformanceExample() throws {
