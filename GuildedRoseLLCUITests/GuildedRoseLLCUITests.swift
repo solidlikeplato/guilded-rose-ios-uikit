220f5a6675a704a5b0b18b535da209a1ea973cee
Malisa Middlebrooks <middlebrooks314@gmail.com>
2021-05-28 10:04:13 -0500

    Add item availability heading

--- GuildedRoseLLCUITests/GuildedRoseLLCUITests.swift
+++ GuildedRoseLLCUITests/GuildedRoseLLCUITests.swift
@@ -10,13 +10,20 @@ class GuildedRoseLLCSpec: QuickSpec {
             app = XCUIApplication()
             app.launch()
         }
-        describe("the message") {
+        describe("the greeting message") {
             context("for the greeting") {
                 it("is 'Welcome to the Guilded Rose'"){
                     let labelElement = app.staticTexts["Greeting"]
                     expect(labelElement.exists).to(beTrue())
                     expect(labelElement.label).to(equal("Welcome to the Guilded Rose LLC!"))
                 }
+            }
+        }
+        describe("Items in stock heading") {
+            it("displays 'Items in stock' text") {
+                let labelElement = app.staticTexts["StockAvailabilityHeading"]
+                expect(labelElement.exists).to(beTrue())
+                expect(labelElement.label).to(equal("Items in stock:"))
             }
         }
     }
