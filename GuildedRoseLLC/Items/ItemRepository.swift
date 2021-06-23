cf7a30dd8af1ba6bb97f827b1d7eeb8d92e3e173
solidlikeplato <ethanwhaley@gmail.com>
2021-06-23 09:01:33 -0500

    extract protocol to separate file

--- GuildedRoseLLC/Items/ItemRepository.swift
+++ GuildedRoseLLC/Items/ItemRepository.swift
@@ -3,26 +3,3 @@ import UIKit
 public protocol ItemRepository {
     func getItems(onSuccess: @escaping (_:[Item]) -> Void)
 }
-
-public class StaticItemRepository : ItemRepository {
-    
-    public init() {}
-    
-    public func getItems(onSuccess: @escaping (_:[Item]) -> Void){
-        var items: [Item] = []
-        
-        let args: String = CommandLine.arguments.count > 1 ? CommandLine.arguments[1] : ""
-        
-        switch args {
-        case "NO_ITEMS_IN_STOCK":
-            items = []
-        case "SINGLE_TEST_ITEM":
-            items = [Item.singleTestItem]
-        case "THREE_TEST_ITEMS":
-            items = [Item.singleTestItem, Item.singleTestItem, Item.singleTestItem,]
-        default:
-            items = Item.testData
-        }
-        onSuccess(items)
-    }
-}
