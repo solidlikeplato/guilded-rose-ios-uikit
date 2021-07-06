aec72a82c84f90e99b2eac1d0b828b514a88f1a1
solidlikeplato <ethanwhaley@gmail.com>
2021-07-06 11:22:08 -0500

    expanded testing for parser

--- GuildedRoseLLCSpec/ItemParserSpec.swift
+++ GuildedRoseLLCSpec/ItemParserSpec.swift
@@ -8,50 +8,113 @@ class ItemParserSpec : QuickSpec {
     override func spec() {
         
         describe("parse function"){
-            it("parses a properly formattted json object into a list of items") {
-                let jsonItems = """
-                    [
-                        {
-                            "id": 24,
-                            "name": "Foo",
-                            "quality": 89,
-                            "sellIn": 67
-                        },
-                        {
-                            "id": 25,
-                            "name": "Bar",
-                            "quality": 8,
-                            "sellIn": 7
-                        }
-                    ]
-                """
-                let data = Data(jsonItems.utf8)
+            context("properly formatted JSON"){
                 
-                let parsedData = ItemParser.parse(json: data)
+                it("parses a empty list into an empty list") {
+                    let jsonItems = """
+                        [
+                        ]
+                    """
+                    let data = Data(jsonItems.utf8)
+                    
+                    let parsedData = ItemParser.parse(json: data)
 
-                expect(parsedData).to(equal([Item(name: "Foo"), Item(name: "Bar")]))
+                    expect(parsedData).to(equal([]))
+                }
+                it("parses a list of one named item into a list of one item") {
+                    let jsonItems = """
+                        [
+                            {
+                                "id": 24,
+                                "name": "Foo",
+                                "quality": 89,
+                                "sellIn": 67
+                            }
+                        ]
+                    """
+                    let data = Data(jsonItems.utf8)
+                    
+                    let parsedData = ItemParser.parse(json: data)
+
+                    expect(parsedData).to(equal([Item(name: "Foo")]))
+                }
+                
+                it("parses a list of many named items into a list of items") {
+                    let jsonItems = """
+                        [
+                            {
+                                "id": 24,
+                                "name": "Foo",
+                                "quality": 89,
+                                "sellIn": 67
+                            },
+                            {
+                                "id": 25,
+                                "name": "Bar",
+                                "quality": 8,
+                                "sellIn": 7
+                            }
+                        ]
+                    """
+                    let data = Data(jsonItems.utf8)
+                    
+                    let parsedData = ItemParser.parse(json: data)
+
+                    expect(parsedData).to(equal([Item(name: "Foo"), Item(name: "Bar")]))
+                }
+                
             }
-            
-            it("returns an empty list for an improperly formatted JSON object") {
-                let jsonItemsWithNoName = """
-                    [
-                        {
-                            "id": 24,
-                            "quality": 89,
-                            "sellIn": 67
-                        },
-                        {
-                            "id": 25,
-                            "quality": 8,
-                            "sellIn": 7
-                        }
-                    ]
-                """
-                let data = Data(jsonItemsWithNoName.utf8)
-                
-                let parsedData = ItemParser.parse(json: data)
+            context("improperly formatted JSON object") {
+                it("parses a list with no named items into an empty list") {
+                    let jsonItemsWithNoName = """
+                        [
+                            {
+                                "id": 24,
+                                "quality": 89,
+                                "sellIn": 67
+                            },
+                            {
+                                "id": 25,
+                                "quality": 8,
+                                "sellIn": 7
+                            }
+                        ]
+                    """
+                    let data = Data(jsonItemsWithNoName.utf8)
+                    
+                    let parsedData = ItemParser.parse(json: data)
 
-                expect(parsedData).to(equal([]))
+                    expect(parsedData).to(equal([]))
+                }
+                
+                it("parses a empty string into an empty list") {
+                    let jsonEmptyString = ""
+                    let data = Data(jsonEmptyString.utf8)
+                    
+                    let parsedData = ItemParser.parse(json: data)
+
+                    expect(parsedData).to(equal([]))
+                }
+                
+                it("parses a gibberish string into an empty list") {
+                    let gibberishString = """
+                        5PePDoEiJC
+                        NMD4xFaDKQ
+                        SRbbTxw0Qz
+                        IGEDHy36il
+                        fvgSzg1Vsf
+                        JJ5XuWjpAI
+                        lsRec5lyrr
+                        A6zYMO6Ozl
+                        CwfNM07HXw
+                        9kHcoImZsK
+                        """
+                    let data = Data(gibberishString.utf8)
+                    
+                    let parsedData = ItemParser.parse(json: data)
+                    
+                    expect(parsedData).to(equal([]))
+                }
             }
         }
     }
