b559c10037ed5d72611b9c4df2fba6c1a07eb4da
Malisa <43938783+Middlebrooks314@users.noreply.github.com>
2021-07-13 12:05:59 -0500

    Revert "Add detail show page"

--- GuildedRoseLLC/Items/ItemCollectionViewDataSource.swift
+++ GuildedRoseLLC/Items/ItemCollectionViewDataSource.swift
@@ -16,7 +16,6 @@ public class ItemCollectionViewDataSource: NSObject, UICollectionViewDataSource 
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ItemCollectionViewCell
         cell.itemCellLabel.text = items[indexPath.row].name
-        cell.itemDetailButton.tag = indexPath.row
 
         return cell
     }
