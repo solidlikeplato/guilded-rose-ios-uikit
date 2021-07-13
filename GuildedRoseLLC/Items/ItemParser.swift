5941c40684e55d0181b1409f478722dc305da56f
Malisa Middlebrooks <middlebrooks314@gmail.com>
2021-07-13 11:44:02 -0500

    update ui

--- GuildedRoseLLC/Items/ItemParser.swift
+++ GuildedRoseLLC/Items/ItemParser.swift
@@ -5,7 +5,6 @@ public class ItemParser {
     
     public static func parse(json: Data) -> [Item] {
         let decoder = JSONDecoder()
-        print("ITEMS LIST BEING DECODED.....")
         if let itemsList: [Item] = try? decoder.decode ([Item].self, from: json) {
             return itemsList
         }
